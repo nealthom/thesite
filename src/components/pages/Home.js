483303d2f5badab77ae5b32be5081361822b9e31
nealthom <nealthom@gmail.com>
2020-08-28 03:20:07 -0500

    fixed typo

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -32,7 +32,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          LOVT
+          LOViT
         </Styledlink>{" "}
         trail in Hot Springs, AR
       </Content>
