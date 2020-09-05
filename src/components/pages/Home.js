58d76336d5c9dd795e441d009c7481858c4158de
nealthom <nealthom@gmail.com>
2020-09-05 06:17:30 -0500

    update home

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -24,7 +24,7 @@ export default function Home() {
   return (
     <PageLayout>
       <Content>
-        Welcome to my world. It's under heavy construction. 8/30/20
+        Welcome to my world. It's under heavy construction. 9/5/20
         <Styledimage src={hike} alt="hiking image" />
         I'm a hiking fanatic. This is from the{" "}
         <Styledlink
