3d88c4572a83b8f9ac4850ef36fced9a7a63006d
Thomas Neal <nealthom@gmail.com>
2020-09-30 02:07:13 -0500

    post action: get individual post by id

--- src/actions/post.js
+++ src/actions/post.js
@@ -1,5 +1,11 @@
 import axios from "axios";
-import { GET_POSTS, POST_ERROR, ADD_POST, ADD_COMMENT } from "./types";
+import {
+  GET_POSTS,
+  POST_ERROR,
+  ADD_POST,
+  GET_POST,
+  ADD_COMMENT
+} from "./types";
 
 export const getPosts = () => async (dispatch) => {
   try {
@@ -26,6 +32,24 @@ export const addPost = (formData) => async (dispatch) => {
     );
     dispatch({
       type: ADD_POST,
+      payload: res.data
+    });
+  } catch (error) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { error }
+    });
+  }
+};
+
+export const getPost = (postId) => async (dispatch) => {
+  try {
+    const res = await axios.get(
+      `https://arcane-oasis-30423.herokuapp.com/posts/${postId}`
+    );
+
+    dispatch({
+      type: GET_POST,
       payload: res.data
     });
   } catch (error) {
