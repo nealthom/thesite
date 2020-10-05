e0cd2a772be71453ff3459f634f2e62e3395727d
Thomas Neal <nealthom@gmail.com>
2020-10-05 04:27:00 -0500

    styled div for Textbox

---
+++ src/components/common/TextBox.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const TextBox = styled.div`
+  font-size: 2.5rem;
+  background: black;
+  border: white solid 3px;
+  background-size: cover;
+  border-radius: 5px;
+  padding: 2rem;
+  @media (min-width: 768px) {
+    font-size: 3.25rem;
+  }
+`;
+
+export { TextBox };
